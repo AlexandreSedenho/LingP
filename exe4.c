13f89bd8fdb09dcf619ee30a53cc5415f6435e8f
AlexandreSedenho <xadinhosedenho@gmail.com>
2025-02-25 21:52:57 -0300

    25-02-2025

--- exe4.c
+++ exe4.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  char letra = 'Y';
-    char proximaY = letra + 0;
-  printf("Letra: %c\n", proximaY);
-  char proxima = letra + 1;
-  printf("Letra: %c\n", proxima);
-    char proxima1 = letra + 2;
-  printf("Letra: %c\n", proxima1);
-    char proxima2 = letra + 3;
-  printf("Letra: %c\n", proxima2);
-    char proxima3 = letra + 4;
-  printf("Letra: %c\n", proxima3);
-    char proxima4 = letra + 5;
-  printf("Letra: %c\n", proxima4);
-    char proxima5 = letra + 6;
-  printf("Letra: %c\n", proxima5);
-    char proxima6 = letra + 7;
-  printf("Letra: %c\n", proxima6);
-    char proxima7 = letra + 8;
-  printf("Letra: %c\n", proxima7);
-    char proxima8 = letra + 9;
-  printf("Letra: %c\n", proxima8);
-    char proxima9 = letra + 10;
-  printf("Letra: %c\n", proxima9);
-  return 0;
-}  
+    int x;
+    
+    printf("Digite um número: ");
+    scanf("%d", &x);
+    
+    int divisao = x / 2;
+    int resto = x % 2;
+
+    if( resto == 1 ) {
+        printf("%d é ímpar", x);
+        return 0;
+    }
+    
+    if( resto == 0 ) {
+        printf("%d é par", x);
+        return 0;
+    }
+    
+    return 0;
+} 
